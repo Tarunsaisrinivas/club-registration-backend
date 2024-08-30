024dde00bb349f065f288965c647e09987183fa1
Tarun Sai Srinivas <tarunsaisrinivas7@gmail.com>
2024-08-30 08:03:34 +0530

    cors update

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173","https://scc-hackoverflow.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 app.use(bodyParser.json());
 
-
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Routes
-const studentRoutes = require('./routes/Student');
-app.use('/api/students', studentRoutes);
+const studentRoutes = require("./routes/Student");
+app.use("/api/students", studentRoutes);
 
-app.use('/', (req, res) => {
-    res.send("Hi");
+app.use("/", (req, res) => {
+  res.send("Hi");
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
